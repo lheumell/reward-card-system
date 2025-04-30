5a37859cc6f1127ed4477cba03d47987c8c7d03e
lheumel <lheumel@mindbaz.com>
2025-04-30 14:31:30 +0200

    feat: delete data instruction

---
+++ app/data-deletion-instructions/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const DeletionDataFacebookPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Suppression des données Facebook</h1>
+      <p>
+        Si vous souhaitez supprimer les données que notre application a
+        collectées via Facebook Login, vous pouvez en faire la demande en nous
+        contactant à l’adresse suivante :
+      </p>
+      <p>
+        <strong>email@tonsite.com</strong>
+      </p>
+      <p>Nous traiterons votre demande dans un délai de 30 jours.</p>
+    </div>
+  );
+};
+
+export default DeletionDataFacebookPage;
