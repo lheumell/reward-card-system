e5bd2912d07bd36844fa3d4b65cab70fdac06fef
lheumel <lheumel@mindbaz.com>
2025-05-22 11:35:40 +0200

    feat:

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,8 +1,20 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/my-account"); // ou une autre page
+  }
+
   return (
     <div className="max-w-7xl flex flex-col gap-12 justify-center items-center my-12 bg-[url(../assets/bg.png)] bg-cover bg-repeat-y bg-center  h-[calc(100vh-300px)]">
       {children}
